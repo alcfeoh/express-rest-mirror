39d4602c6bb4218db349d614624e2cb909b7b5e3
alcfeoh <alain@interstate21.com>
2022-12-13 15:51:12 -0800

    Mirror API working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var cors = require('cors');
-const port = 80;
+const port = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(cors());
